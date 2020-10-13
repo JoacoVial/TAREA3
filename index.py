5907eb322abede0a1b2d8fdfa26c1db6ad9a66c0
JoacoVial <64756805+JoacoVial@users.noreply.github.com>
2020-10-13 16:48:31 -0300

    Update index.py

--- index.py
+++ index.py
@@ -4,28 +4,32 @@ from Crypto.Cipher import AES
 
 BS = 16
 pad = lambda s: bytes(s + (BS - len(s) % BS) * chr(BS - len(s) % BS), 'utf-8')
-unpad = lambda s : s[0:-ord(s[-1:])]
 
 class AESCipher:
 
     def __init__( self, key ):
-        self.key = bytes(key)
+        self.key = key
 
-    def encrypt( self, raw ):
-        raw = pad(raw)
-        iv = 16 * b'\x00'
+    def encrypt( self, msg ):
+        msg = pad(msg)
+        iv =  b'\x01\x23\x45\x67\x89\xab\xcd\xef\x01\x23\x45\x67\x89\xab\xcd\xef'#16 * b'\x00'
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return base64.b64encode(cipher.encrypt( raw ) )
+        return base64.b64encode(cipher.encrypt( msg ) )
 
-key = 16
+
+key = b'\x01\x23\x45\x67\x89\xab\xcd\xef\x01\x23\x45\x67\x89\xab\xcd\xef'
+key_js = "0123456789abcdef0123456789abcdef"
+iv_js = "0123456789abcdef0123456789abcdef"
+#hex_key = hex + key[0] + key[1] + hex + key[2] + key[3] + hex + key[4] + key[5] + hex + key[6] + key[7] + hex + key[8] + key[9] + hex + key[10] + key[11] + hex + key[12] + key[13] + hex + key[14] + key[15]
+msg = "y"
+
 cipher = AESCipher(key)
-iv = "00000000000000000000000000000000"
-encrypted = cipher.encrypt('criptografia2020').decode('utf8') #se pasa el mensaje criptografia en utf-8
+encrypted = cipher.encrypt(msg).decode('utf8') #se pasa el mensaje criptografia en utf-8
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('desciframe.html', variable = encrypted, variable2 = iv)
+    return render_template('desciframe.html', msg = encrypted, iv = iv_js, key = key_js)
 
 if __name__ == '__main__':
     app.run(debug=True)
